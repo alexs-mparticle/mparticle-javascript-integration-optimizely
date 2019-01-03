9e99e4749dd20a8c06cbf09425dc146b83144ba8
Robert Ing <ring@mparticle.com>
2019-01-03 14:43:27 -0500

    test-end-to-end

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-module.exports = app;
-var server = app.listen(8082, function() {
-    console.log(__dirname);
-    console.log('express server listening on port ' + server.address().port);
-});
